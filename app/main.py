a4becd81df3989e20938d1516161cd561218ccec
Wilson Tsai <wilson.tsai@highercloud.com.tw>
2024-10-22 14:52:01 +0800

    chore: 依照Black修改縮排。

--- app/main.py
+++ app/main.py
@@ -23,10 +23,8 @@ app = FastAPI(
     version="1.0.0",
 )
 
-db_config = {
-    **dotenv_values(".env"),
-    **dotenv_values(".env.example")
-}
+db_config = {**dotenv_values(".env"), **dotenv_values(".env.example")}
+
 
 # TODO: model object than ODM to Mongodb.
 class Whisky(BaseModel):
@@ -36,16 +34,20 @@ class Whisky(BaseModel):
     abv: float
     volume: int
 
+
 class Customer(BaseModel):
     name: str
     phone_number: str
     email: Optional[str]
-    
+
+
 class Staff(BaseModel):
     name: str
-    
+
+
 class Record(BaseModel):
     id: int
+
 
 @app.get("/", tags=["Home"])
 def home():
@@ -54,76 +56,73 @@ def home():
         "Version": "1.0.0",
     }
 
+
 def open_conn(
-    username = db_config["MONGO_INITDB_ROOT_USERNAME"],
-    password = db_config["MONGO_INITDB_ROOT_PASSWORD"],
-    host = "whisky-map-mongodb",
-    port = 27017,
-    db = "",
-    table = "",
+    username=db_config["MONGO_INITDB_ROOT_USERNAME"],
+    password=db_config["MONGO_INITDB_ROOT_PASSWORD"],
+    host="whisky-map-mongodb",
+    port=27017,
+    db="",
+    table="",
 ):
-  uri = "mongodb://%s:%s@%s:%s/" % (username, password, host, port)
-  conn = pymongo.MongoClient(uri)
-  return conn[db][table]
+    uri = "mongodb://%s:%s@%s:%s/" % (username, password, host, port)
+    conn = pymongo.MongoClient(uri)
+    return conn[db][table]
+
 
 @app.post("/whiskys", tags=["Whisky"])
 async def create_whisky(whisky: Whisky):
     data = whisky.model_dump()
-    
+
     collection_whisky = open_conn(db="app", table="whisky")
-    
+
     result = collection_whisky.insert_one(data)
     if not result.acknowledged:
         logging.warning(f"{whisky.name} 寫入失敗！\n")
         print(f"{whisky.name} 寫入失敗！\n")
-    
-    return JSONResponse(
-        content={
-            "message": f"{whisky.name} 成功寫入資料庫。"
-        }
-    )
-    
+
+    return JSONResponse(content={"message": f"{whisky.name} 成功寫入資料庫。"})
+
+
 @app.get("/whiskys", tags=["Whisky"])
 async def get_whisky():
     collection_whisky = open_conn(db="app", table="whisky")
     cursor = collection_whisky.find()
     df_result = pd.DataFrame(list(cursor))
-    
+
     if df_result.empty:
         return {"message": "whisky data not found."}
-    
-    del df_result['_id']
+
+    del df_result["_id"]
     json_result = df_result.to_json(orient="records")
-    
+
     return json.loads(json_result)
-    
+
+
 @app.post("/customers", tags=["Customer"])
 async def create_customer(customer: Customer):
     data = customer.model_dump()
-    
+
     collection_customer = open_conn(db="app", table="customer")
-    
+
     result = collection_customer.insert_one(data)
     if not result.acknowledged:
         logging.warning(f"{customer.name} 寫入失敗！\n")
         print(f"{customer.name} 寫入失敗！\n")
-    
-    return JSONResponse(
-        content={
-            "message": f"{customer.name} 成功寫入資料庫。"
-        }
-    )
-    
+
+    return JSONResponse(content={"message": f"{customer.name} 成功寫入資料庫。"})
+
+
 @app.get("/customers", tags=["Customer"])
 async def get_customer():
     collection_customer = open_conn(db="app", table="customer")
     cursor = collection_customer.find()
     df_result = pd.DataFrame(list(cursor))
-    
+
     if df_result.empty:
         return {"message": "customer data not found."}
-    
-    del df_result['_id']
+
+    del df_result["_id"]
     json_result = df_result.to_json(orient="records")
-    
+
     return json.loads(json_result)
